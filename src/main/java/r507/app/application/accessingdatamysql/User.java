41600d4870ac4f35d6edeb5b13a348b9deadfea6
teo <asselineteo@gmail.com>
2023-11-21 10:41:31 +0100

    db-ready

---
+++ src/main/java/r507/app/application/accessingdatamysql/User.java
@@ -0,0 +1,44 @@
+package r507.app.application.accessingdatamysql;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+
+  private Integer id;
+  private String name;
+  private String mdp;
+  private String role;
+
+  public Integer getId() {
+    return id;
+  }
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getMdp() {
+    return mdp;
+  }
+  public void setMdp(String mdp) {
+    this.mdp = mdp;
+  }
+
+  public String getRole() {
+    return role;
+  }
+  public void setRole(String role) {
+    this.role = role;
+  }
+}
