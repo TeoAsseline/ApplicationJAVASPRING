3b2c01d632843c76370acb3f5ba7973c35221ac8
teo <asselineteo@gmail.com>
2023-11-21 09:16:14 +0100

    first commit

---
+++ src/main/java/r507/app/application/Application.java
@@ -0,0 +1,21 @@
+package r507.app.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@RequestMapping("/bonjour")
+    String home() {
+        return "Bonjour le monde !";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
