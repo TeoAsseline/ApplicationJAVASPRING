e866db16ad1f3064aab43f537d1747b1baa97727
teo <asselineteo@gmail.com>
2023-11-21 09:18:46 +0100

    hello-world

--- src/main/java/r507/app/application/Application.java
+++ src/main/java/r507/app/application/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/bonjour")
     String home() {
-        return "Bonjour le monde !";
+        return "HELLO WORLD - Bonjour le monde !";
     }
 
 	public static void main(String[] args) {
