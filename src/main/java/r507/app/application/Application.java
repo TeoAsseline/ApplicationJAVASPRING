9054da5e4638ac887590969d252ee49e2e450b64
teo <asselineteo@gmail.com>
2023-12-12 16:21:02 +0100

    Debut Tentative Authentification

--- src/main/java/r507/app/application/Application.java
+++ src/main/java/r507/app/application/Application.java
@@ -2,8 +2,12 @@ package r507.app.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import r507.app.application.accessingdatamysql.Auteur;
 
 @RestController
 @SpringBootApplication
@@ -13,7 +17,13 @@ public class Application {
     String home() {
         return "HELLO WORLD - Bonjour le monde !";
     }
-	
+
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+    model.addAttribute("user", new Auteur());
+    return "signup_form";
+}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
